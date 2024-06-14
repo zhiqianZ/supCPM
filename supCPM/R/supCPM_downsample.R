f76ff8025427044e530edce92ba9a9e4a8e14a9f
zhiqianZ <63725891+zhiqianZ@users.noreply.github.com>
2024-06-14 01:52:03 -0700

    Add files via upload
    Update the supCPM_downsample.R

--- supCPM/R/supCPM_downsample.R
+++ supCPM/R/supCPM_downsample.R
@@ -4,10 +4,13 @@
 #' supCPM requires both high dimensional data and label information as input. Labels could be obtained by clustering algorithm or experiments.
 #' With the adjustment of parameters, supCPM could also output the result of CPM (Capacity Preserving Mapping).
 #'
+#' supCPM downsample use Geometric sketching to downsample a subset of cells to run the supCPM algoirthm, and the remaining unselected cell's visualization coordinates will be projected the center of its kNN among the downsampled cells plus a random perturbation.
+#'
+#' Prior to run this function, you need to install reticulate and python package geoskech first.
 #'
 #' @param data matrix; Input data matrix with rows representing cells and columns representing genes.
 #' @param label vector; The label vector indicating which cluster that each cell belongs to.
-#' @param alpha numeric; The quantile of distance pairs, which is used to determine the threshold for downsampling.
+#' @param alpha numeric; The proportion of cells to downsample (default is 0.1).
 #' @param ratio numeric between 0 and 1; This is a trade-off between the KL-divergence and trace ratio term.
 #' @param no_dims integer; The dimensionality of the embedding space (default is 2).
 #' @param compel_force 0 or 1; Compel_force equals to 0 if user wants to pull clusters a bit apart, and equals to 1 if user wants the best preservation of the geometry (default is 0).
@@ -19,62 +22,62 @@
 #' @param seed integer; Random seed for supCPM. (default is 40)
 #' @param factor numeric; The factor multiples on the high-dimensional distance matrix between different clusters.
 #' @param verbose logical; Whether the progress of the objective function should be printed (default is TRUE).
-#' @param init logical; Whether to ues the random initialization or not. If the init=T,
+#' @param init logical; Whether to use the random initialization or not. If the init=T,
 #'              then the initialization will be the MDS of the adjusted capacity distance matrix. (default is FALSE)
+#' @param return.idx logical; Whether to return the indices of cells that are downsampled to actually run the supCPM algorithm (default is FALSE).
 #' @param epsilon numeric; The multiple of the default epsilon, which is used in converting distance to probability; Default value is 1.
 #' @param lr numeric; The learning rate for optimization. Default value is 500.
-#'
 #' @return Returns an matrix containing supCPM coordinates in the embedding dimenisons.
 #' @references Zhiqian Zhai, Yu L. Lei, Rongrong Wang, Yuying Xie, Supervised Capacity Preserving Mapping: A Clustering Guided Visualization Method for scRNAseq data,
 #' bioRxiv 2021.06.18.448900; doi: https://doi.org/10.1101/2021.06.18.448900
+#' @references geometric sketching algorithm described by Brian Hie, Hyunghoon Cho, Benjamin DeMeo, Bryan Bryson, and Bonnie Berger in "Geometric sketching compactly summarizes the single-cell transcriptomic landscape", Cell Systems (2019).
 #' @export
-supCPM_downsample = function(data,label,alpha =0.02,no_dims=2,compel_force=0,dist='euclidean',degree=2,ratio,k=7,niter1=700,niter2=700,seed=42,factor=1.3,verbose=T,init=T){
-  n = nrow(data)
-  D = sqDist(data)
-  I = order(D[,])
-  base = ceiling(length(I)*alpha)
-  base_id = arrayInd(base,c(n,n))
-  threshold = D[ I[base] ]
-  merge = rep(0,n)
-  indtable = matrix(0,n,2)
-  cl_size =  table(label)
-  large_cluster = which(cl_size>n*0.05)
-  large_cluster = sort(unique(label))[large_cluster]
-  k=1
-  for (i in large_cluster){
-    cl = which(label==i)
-    for( j in 1:length(cl) ){
-      if( merge[cl[j]] == 0){ # whether the point is reomved
-        smallind = which(D[cl[j],cl]< threshold )
-        smallind = intersect(smallind, which(merge[cl]==0))
-        if ( length(smallind) > 1){
-          merge[cl[smallind]] = k
-          indtable[k,] = c(cl[j],k)
-          k=k+1
-        }
-      }
-    }
+#' @import RcppAnnoy
+#' @import reticulate
+supCPM_downsample = function(data,label,alpha =0.1,
+                             no_dims=2,compel_force=0, dist='euclidean',
+                             degree=2,ratio,k=7,niter1=700,niter2=700,seed=42,factor=1.3,epsilon=1,lr=500,
+                             verbose=T,init=T,return.idx=F){
+  library(RcppAnnoy)
+  n = length(label)
+  set.seed(seed)
+  geosketch <-  reticulate::import('geosketch')
+  sketch.size <- as.integer(n*alpha)
+  sketch.indices <- geosketch$gs(data, sketch.size)
+  sketch.indices = unlist(sketch.indices)
+  if(sum(table(label[sketch.indices])<10)>0){
+    #if there is any cell types contains fewer than 10 cells after downsampling, use all
+    celltype = names(which(table(label[sketch.indices]))<10)
+    sketch.indices = union(sketch.indices,which(label%in%celltype))
   }
-  indtable = indtable[-which(indtable[,2]==0),]
-  ind = indtable[,1]; ind = c(ind,which(merge==0))
-  data_downsample = data[ind,]
-  label_downsample = label[ind]
-  print(dim(data_downsample))
-  embed = supCPM(data_downsample,label_downsample,no_dims=no_dims,compel_force=compel_force,dist=dist,degree=degree,ratio=ratio,k=k,niter1=niter1,niter2=niter2,seed=seed,factor=factor,verbose=verbose,init=init)
+  data_downsample = data[sketch.indices,]
+  label_downsample = as.vector(label[sketch.indices])
+  embed = supCPM(data_downsample,label_downsample,no_dims=no_dims,compel_force=compel_force,
+                 dist=dist,degree=degree,ratio=ratio,k=k,niter1=niter1,niter2=niter2,
+                 seed=seed,factor=factor,verbose=verbose,init=init,
+                 epsilon=epsilon,lr=lr
+                 )
   embed_recover = matrix(0,n,no_dims)
-  embed_recover[ind,] = embed
-  rsmall = dist(embed_recover[base_id,])
-  D = sqDist(embed)
-  diag(D)=0
-  rsmall = sqrt(min(D[D!=0]))
-  for ( i in 1:nrow(indtable)){
-    i = 1
-    cl = which(merge==i)
-    center = indtable[i,1]
-    random = matrix(rnorm(length(cl)*no_dims),ncol=no_dims)
-    #id = which(abs(random)>1)
-    #random[id] = sign(random[id])
-    embed_recover[cl,] = matrix(rep(embed_recover[center,],length(cl)),ncol=no_dims,byrow = T) + random * sqrt(rsmall/2)
+  embed_recover[sketch.indices,] = embed
+  select = rep(0, length(label))
+  select[sketch.indices] = "sketched cell"
+  select[-sketch.indices] = "projected cell"
+  for( i in unique(label) ){
+    idx = which(label==i)
+    idx_subsampled = idx[idx%in%sketch.indices]
+    idx_new = idx[!idx%in%sketch.indices]
+    d = as.matrix(distances::distances(embed[label_downsample==i,]))
+    min_dist = min(d[d!=0])
+    Annoy <- new(AnnoyEuclidean, ncol(data[idx_subsampled,]))
+    Annoy$setSeed(seed)
+    for (i in 1:length(idx_subsampled)) Annoy$addItem(i-1, data[idx_subsampled[i],])
+    Annoy$build(50)
+    neighbor.size = min(10, length(idx_subsampled))
+    NN = t(sapply(1:length(idx_new), function(i) idx_subsampled[Annoy$getNNsByVector(as.vector(data[idx_new[i],]), neighbor.size+1)+1]))
+    embed_recover[idx_new, ] = t(apply(NN,1,function(idx) colMeans(embed_recover[idx,]))) + rnorm(1,0, mean = sqrt(min_dist/2))
+  }
+  if(return.idx==T){
+    return(list(supCPM=embed_recover,idx=select))
   }
   return(embed_recover)
 }
